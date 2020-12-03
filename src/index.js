4e893c792dd1c047276ddb460c7cd06a5d3a6487
Rohan Bhangui <bhanguirohan@gmail.com>
2020-12-03 01:15:16 -0500

    proper semi colon

--- src/index.js
+++ src/index.js
@@ -5,14 +5,14 @@ document.addEventListener("DOMContentLoaded", () => {
   const LOADING_TIMER = 500;
   setTimeout(() => {
     document.querySelector(".numbers-1").classList.add("active");
-  }, 500)
+  }, 500);
   setTimeout(() => {
     document.querySelector(".numbers-2").classList.add("active");
-  }, LOADING_TIMER+1000)
+  }, LOADING_TIMER+1000);
   setTimeout(() => {
     document.querySelector(".sounds-text").classList.add("active");
-  }, LOADING_TIMER+1750)
+  }, LOADING_TIMER+1750);
   setTimeout(() => {
     document.querySelector(".social").classList.add("active");
-  }, LOADING_TIMER+2750)
+  }, LOADING_TIMER+2750);
 });
