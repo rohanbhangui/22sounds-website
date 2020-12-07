5036bb38343c7b8c3b785b6a81dfe6476de607ee
Rohan Bhangui <bhanguirohan@gmail.com>
2020-12-06 20:44:51 -0500

    fixed the mobile menu

--- src/index.js
+++ src/index.js
@@ -10,6 +10,19 @@ require('../node_modules/@glidejs/glide/dist/css/glide.theme.min.css');
 
 document.addEventListener("DOMContentLoaded", () => {
 
+  // for mobile menu open
+  document.querySelector("header.header .mobile-open-button i").addEventListener('click', event => {
+    document.querySelector("header.header .links").classList.add("mobile-open");
+  });
+
+
+  debugger;
+  // for mobile menu close
+  document.querySelector("header.header .links .mobile-close-button i").addEventListener('click', event => {
+    document.querySelector("header.header .links").classList.remove("mobile-open");
+  });
+  
+
   if(document.querySelector("body").classList.contains("index")) {
     const LOADING_TIMER = 500;
     setTimeout(() => {
@@ -24,12 +37,20 @@ document.addEventListener("DOMContentLoaded", () => {
     
     if(window.scrollY >= window.innerHeight/2) {
       document.querySelector("header.header").classList.add("show");
+      document.querySelector("#container-1 video").classList.add("show");
     }
     else {
       setTimeout(() => {
         document.querySelector("header.header").classList.add("show");
+        document.querySelector("#container-1 video").classList.add("show");
       }, LOADING_TIMER+2750);
     }
+
+    window.addEventListener('scroll', (e) => {
+      if(window.scrollY >= window.innerHeight/2) {
+        document.querySelector("header.header").classList.add("show");
+      }
+    });
 
     // const r = rolly({
     //   view: document.querySelector('.container-wrapper'),
@@ -38,7 +59,7 @@ document.addEventListener("DOMContentLoaded", () => {
     // });
     // r.init();
 
-    window.glide = new Glide('#container-3 .glide', {
+    new Glide('#container-3 .glide', {
       type: 'slider',
       perView: 3.5,
       gap: 20,
