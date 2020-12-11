a650b9250454b05d81452a7827d2c6a94be1c88b
Rohan Bhangui <bhanguirohan@gmail.com>
2020-12-11 18:09:15 -0500

    fixed the discography

--- src/pages/discography/discography.js
+++ src/pages/discography/discography.js
@@ -4,66 +4,42 @@ const full = [
     img: "https://is1-ssl.mzstatic.com/image/thumb/Music114/v4/6f/21/09/6f21093c-8e44-95fc-718f-3778bcadbe3a/artwork.jpg/1000x1000bb.webp",
     release_date: "November 25, 2020",
     youtube_embed: "",
-    url: {
-      apple: "https://apple.co/3m2lUk0",
-      spotify: "https://open.spotify.com/album/7Lsnyse35C75WrOMrCz7Wo",
-      soundcloud: "",
-    }
-  },
-  {
-    title: "Lockdown - Single",
-    img: "https://is3-ssl.mzstatic.com/image/thumb/Music113/v4/9b/85/10/9b851068-3ce5-9649-26b3-3fb79ec4df96/artwork.jpg/1000x1000bb.webp",
-    release_date: "April 17, 2020",
-    youtube_embed: "",
-    url: {
-      apple: "https://apple.co/2Lq7vBX",
-      spotify: "https://open.spotify.com/album/0msItxyeb0ZwwUlKesph9Q",
-      soundcloud: "",
-    }
-  },
-  {
-    title: "No Peace - Single",
-    img: "https://is2-ssl.mzstatic.com/image/thumb/Music123/v4/e8/9e/3a/e89e3a59-a92c-2d49-bae8-fb2592da483e/artwork.jpg/1000x1000bb.webp",
-    release_date: "April 19, 2019",
-    youtube_embed: "",
-    url: {
-      apple: "https://apple.co/39VWouH",
-      spotify: "https://open.spotify.com/album/6aDLCT8ycHE3CMOuS5NWfU",
-      soundcloud: "",
-    }
+    url: "https://smarturl.it/5n954q"
   },
   {
     title: "Starburst - Single",
     img: "https://is3-ssl.mzstatic.com/image/thumb/Music124/v4/05/6b/22/056b22a9-b457-7c9c-eff7-8367e0b640d0/artwork.jpg/1000x1000bb.webp",
     release_date: "October 9, 2020",
     youtube_embed: "https://www.youtube.com/embed/-ywU94Dxurs?vq=hd1080&modestbranding=1&theme=light&color=white",
-    url: {
-      apple: "https://apple.co/2K7x8GO",
-      spotify: "https://open.spotify.com/album/4Ni6wmKoMyDDqzNFWX4ThK",
-      soundcloud: "",
-    }
-  },
-  {
-    title: "Two Gold - Single",
-    img: "https://is5-ssl.mzstatic.com/image/thumb/Music123/v4/ad/53/3a/ad533ae1-63f2-33c1-cab1-f97662b89ea2/artwork.jpg/1000x1000bb.webp",
-    release_date: "July 5, 2019",
-    youtube_embed: "",
-    url: {
-      apple: "https://apple.co/39VitcF",
-      spotify: "https://open.spotify.com/album/6Hoa5sH0U8S96dWWftlBqA",
-      soundcloud: "",
-    }
+    url: "https://smarturl.it/lrk3oz"
   },
   {
     title: "Hitting the Moon - Single",
     img: "https://is3-ssl.mzstatic.com/image/thumb/Music123/v4/d7/d1/3a/d7d13ace-1f08-8729-b221-eae063b7940b/artwork.jpg/1000x1000bb.webp",
     release_date: "February 28, 2020",
     youtube_embed: "",
-    url: {
-      apple: "https://apple.co/2VUpfao",
-      spotify: "https://open.spotify.com/album/0EKnF9LLiyTteiOid9wqPX",
-      soundcloud: "",
-    }
+    url: "https://smarturl.it/lzuadr"
+  },
+  {
+    title: "No Peace - Single",
+    img: "https://is2-ssl.mzstatic.com/image/thumb/Music123/v4/e8/9e/3a/e89e3a59-a92c-2d49-bae8-fb2592da483e/artwork.jpg/1000x1000bb.webp",
+    release_date: "April 19, 2019",
+    youtube_embed: "",
+    url: "https://smarturl.it/f0c378"
+  },
+  {
+    title: "Lockdown - Single",
+    img: "https://is3-ssl.mzstatic.com/image/thumb/Music113/v4/9b/85/10/9b851068-3ce5-9649-26b3-3fb79ec4df96/artwork.jpg/1000x1000bb.webp",
+    release_date: "April 17, 2020",
+    youtube_embed: "",
+    url: "#"
+  },
+  {
+    title: "Two Gold - Single",
+    img: "https://is5-ssl.mzstatic.com/image/thumb/Music123/v4/ad/53/3a/ad533ae1-63f2-33c1-cab1-f97662b89ea2/artwork.jpg/1000x1000bb.webp",
+    release_date: "July 5, 2019",
+    youtube_embed: "",
+    url: "#"
   },
 ]
 
