953386f0ae841172aeb3e77d0e68bd0a873f6b3e
Rohan Bhangui <bhanguirohan@gmail.com>
2021-02-10 00:41:40 -0500

    changed assets to absolute path

--- src/pages/video/videography.js
+++ src/pages/video/videography.js
@@ -1,112 +1,112 @@
 const full = [
   {
     title: "Starburst - Single",
-    img: "assets/img/videos/starburst-cover.jpg",
+    img: "/assets/img/videos/starburst-cover.jpg",
     release_date: "October 9, 2020",
     youtube_embed: "https://www.youtube.com/embed/-ywU94Dxurs?vq=hd1080&modestbranding=1&theme=light&color=white",
     url: "https://www.youtube.com/watch?v=-ywU94Dxurs"
   },
   {
     title: "Hitting the Moon - Single",
-    img: "assets/img/videos/hitting-the-moon-cover.jpg",
+    img: "/assets/img/videos/hitting-the-moon-cover.jpg",
     release_date: "February 28, 2020",
     youtube_embed: "https://www.youtube.com/embed/_rlrfH4UKYQ?vq=hd1080&modestbranding=1&theme=light&color=white",
     url: "https://www.youtube.com/watch?v=_rlrfH4UKYQ"
   },
   {
     title: "Starburst - Single",
-    img: "assets/img/videos/starburst-cover.jpg",
+    img: "/assets/img/videos/starburst-cover.jpg",
     release_date: "October 9, 2020",
     youtube_embed: "https://www.youtube.com/embed/-ywU94Dxurs?vq=hd1080&modestbranding=1&theme=light&color=white",
     url: "https://www.youtube.com/watch?v=-ywU94Dxurs"
   },
   {
     title: "Hitting the Moon - Single",
-    img: "assets/img/videos/hitting-the-moon-cover.jpg",
+    img: "/assets/img/videos/hitting-the-moon-cover.jpg",
     release_date: "February 28, 2020",
     youtube_embed: "https://www.youtube.com/embed/_rlrfH4UKYQ?vq=hd1080&modestbranding=1&theme=light&color=white",
     url: "https://www.youtube.com/watch?v=_rlrfH4UKYQ"
   },
   {
     title: "Starburst - Single",
-    img: "assets/img/videos/starburst-cover.jpg",
+    img: "/assets/img/videos/starburst-cover.jpg",
     release_date: "October 9, 2020",
     youtube_embed: "https://www.youtube.com/embed/-ywU94Dxurs?vq=hd1080&modestbranding=1&theme=light&color=white",
     url: "https://www.youtube.com/watch?v=-ywU94Dxurs"
   },
   {
     title: "Hitting the Moon - Single",
-    img: "assets/img/videos/hitting-the-moon-cover.jpg",
+    img: "/assets/img/videos/hitting-the-moon-cover.jpg",
     release_date: "February 28, 2020",
     youtube_embed: "https://www.youtube.com/embed/_rlrfH4UKYQ?vq=hd1080&modestbranding=1&theme=light&color=white",
     url: "https://www.youtube.com/watch?v=_rlrfH4UKYQ"
   },
   {
     title: "Starburst - Single",
-    img: "assets/img/videos/starburst-cover.jpg",
+    img: "/assets/img/videos/starburst-cover.jpg",
     release_date: "October 9, 2020",
     youtube_embed: "https://www.youtube.com/embed/-ywU94Dxurs?vq=hd1080&modestbranding=1&theme=light&color=white",
     url: "https://www.youtube.com/watch?v=-ywU94Dxurs"
   },
   {
     title: "Hitting the Moon - Single",
-    img: "assets/img/videos/hitting-the-moon-cover.jpg",
+    img: "/assets/img/videos/hitting-the-moon-cover.jpg",
     release_date: "February 28, 2020",
     youtube_embed: "https://www.youtube.com/embed/_rlrfH4UKYQ?vq=hd1080&modestbranding=1&theme=light&color=white",
     url: "https://www.youtube.com/watch?v=_rlrfH4UKYQ"
   },
   {
     title: "Starburst - Single",
-    img: "assets/img/videos/starburst-cover.jpg",
+    img: "/assets/img/videos/starburst-cover.jpg",
     release_date: "October 9, 2020",
     youtube_embed: "https://www.youtube.com/embed/-ywU94Dxurs?vq=hd1080&modestbranding=1&theme=light&color=white",
     url: "https://www.youtube.com/watch?v=-ywU94Dxurs"
   },
   {
     title: "Hitting the Moon - Single",
-    img: "assets/img/videos/hitting-the-moon-cover.jpg",
+    img: "/assets/img/videos/hitting-the-moon-cover.jpg",
     release_date: "February 28, 2020",
     youtube_embed: "https://www.youtube.com/embed/_rlrfH4UKYQ?vq=hd1080&modestbranding=1&theme=light&color=white",
     url: "https://www.youtube.com/watch?v=_rlrfH4UKYQ"
   },
   {
     title: "Starburst - Single",
-    img: "assets/img/videos/starburst-cover.jpg",
+    img: "/assets/img/videos/starburst-cover.jpg",
     release_date: "October 9, 2020",
     youtube_embed: "https://www.youtube.com/embed/-ywU94Dxurs?vq=hd1080&modestbranding=1&theme=light&color=white",
     url: "https://www.youtube.com/watch?v=-ywU94Dxurs"
   },
   {
     title: "Hitting the Moon - Single",
-    img: "assets/img/videos/hitting-the-moon-cover.jpg",
+    img: "/assets/img/videos/hitting-the-moon-cover.jpg",
     release_date: "February 28, 2020",
     youtube_embed: "https://www.youtube.com/embed/_rlrfH4UKYQ?vq=hd1080&modestbranding=1&theme=light&color=white",
     url: "https://www.youtube.com/watch?v=_rlrfH4UKYQ"
   },
   {
     title: "Starburst - Single",
-    img: "assets/img/videos/starburst-cover.jpg",
+    img: "/assets/img/videos/starburst-cover.jpg",
     release_date: "October 9, 2020",
     youtube_embed: "https://www.youtube.com/embed/-ywU94Dxurs?vq=hd1080&modestbranding=1&theme=light&color=white",
     url: "https://www.youtube.com/watch?v=-ywU94Dxurs"
   },
   {
     title: "Hitting the Moon - Single",
-    img: "assets/img/videos/hitting-the-moon-cover.jpg",
+    img: "/assets/img/videos/hitting-the-moon-cover.jpg",
     release_date: "February 28, 2020",
     youtube_embed: "https://www.youtube.com/embed/_rlrfH4UKYQ?vq=hd1080&modestbranding=1&theme=light&color=white",
     url: "https://www.youtube.com/watch?v=_rlrfH4UKYQ"
   },
   {
     title: "Starburst - Single",
-    img: "assets/img/videos/starburst-cover.jpg",
+    img: "/assets/img/videos/starburst-cover.jpg",
     release_date: "October 9, 2020",
     youtube_embed: "https://www.youtube.com/embed/-ywU94Dxurs?vq=hd1080&modestbranding=1&theme=light&color=white",
     url: "https://www.youtube.com/watch?v=-ywU94Dxurs"
   },
   {
     title: "Hitting the Moon - Single",
-    img: "assets/img/videos/hitting-the-moon-cover.jpg",
+    img: "/assets/img/videos/hitting-the-moon-cover.jpg",
     release_date: "February 28, 2020",
     youtube_embed: "https://www.youtube.com/embed/_rlrfH4UKYQ?vq=hd1080&modestbranding=1&theme=light&color=white",
     url: "https://www.youtube.com/watch?v=_rlrfH4UKYQ"
