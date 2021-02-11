e97386ccff9d4f2c112dcdcec26da0e2f4e2feca
Rohan Bhangui <bhanguirohan@gmail.com>
2021-02-10 23:24:25 -0500

    made images cloudinary based

--- src/pages/video/videography.js
+++ src/pages/video/videography.js
@@ -1,112 +1,112 @@
 const full = [
   {
     title: "Starburst - Single",
-    img: "/assets/img/videos/starburst-cover.jpg",
+    img: "https://res.cloudinary.com/a22sounds/image/upload/v1613017396/videos/starburst-cover_jxvfdr.jpg",
     release_date: "October 9, 2020",
     youtube_embed: "https://www.youtube.com/embed/-ywU94Dxurs?vq=hd1080&modestbranding=1&theme=light&color=white",
     url: "https://www.youtube.com/watch?v=-ywU94Dxurs"
   },
   {
     title: "Hitting the Moon - Single",
-    img: "/assets/img/videos/hitting-the-moon-cover.jpg",
+    img: "https://res.cloudinary.com/a22sounds/image/upload/v1613017396/videos/hitting-the-moon-cover_yfmrho.jpg",
     release_date: "February 28, 2020",
     youtube_embed: "https://www.youtube.com/embed/_rlrfH4UKYQ?vq=hd1080&modestbranding=1&theme=light&color=white",
     url: "https://www.youtube.com/watch?v=_rlrfH4UKYQ"
   },
   {
     title: "Starburst - Single",
-    img: "/assets/img/videos/starburst-cover.jpg",
+    img: "https://res.cloudinary.com/a22sounds/image/upload/v1613017396/videos/starburst-cover_jxvfdr.jpg",
     release_date: "October 9, 2020",
     youtube_embed: "https://www.youtube.com/embed/-ywU94Dxurs?vq=hd1080&modestbranding=1&theme=light&color=white",
     url: "https://www.youtube.com/watch?v=-ywU94Dxurs"
   },
   {
     title: "Hitting the Moon - Single",
-    img: "/assets/img/videos/hitting-the-moon-cover.jpg",
+    img: "https://res.cloudinary.com/a22sounds/image/upload/v1613017396/videos/hitting-the-moon-cover_yfmrho.jpg",
     release_date: "February 28, 2020",
     youtube_embed: "https://www.youtube.com/embed/_rlrfH4UKYQ?vq=hd1080&modestbranding=1&theme=light&color=white",
     url: "https://www.youtube.com/watch?v=_rlrfH4UKYQ"
   },
   {
     title: "Starburst - Single",
-    img: "/assets/img/videos/starburst-cover.jpg",
+    img: "https://res.cloudinary.com/a22sounds/image/upload/v1613017396/videos/starburst-cover_jxvfdr.jpg",
     release_date: "October 9, 2020",
     youtube_embed: "https://www.youtube.com/embed/-ywU94Dxurs?vq=hd1080&modestbranding=1&theme=light&color=white",
     url: "https://www.youtube.com/watch?v=-ywU94Dxurs"
   },
   {
     title: "Hitting the Moon - Single",
-    img: "/assets/img/videos/hitting-the-moon-cover.jpg",
+    img: "https://res.cloudinary.com/a22sounds/image/upload/v1613017396/videos/hitting-the-moon-cover_yfmrho.jpg",
     release_date: "February 28, 2020",
     youtube_embed: "https://www.youtube.com/embed/_rlrfH4UKYQ?vq=hd1080&modestbranding=1&theme=light&color=white",
     url: "https://www.youtube.com/watch?v=_rlrfH4UKYQ"
   },
   {
     title: "Starburst - Single",
-    img: "/assets/img/videos/starburst-cover.jpg",
+    img: "https://res.cloudinary.com/a22sounds/image/upload/v1613017396/videos/starburst-cover_jxvfdr.jpg",
     release_date: "October 9, 2020",
     youtube_embed: "https://www.youtube.com/embed/-ywU94Dxurs?vq=hd1080&modestbranding=1&theme=light&color=white",
     url: "https://www.youtube.com/watch?v=-ywU94Dxurs"
   },
   {
     title: "Hitting the Moon - Single",
-    img: "/assets/img/videos/hitting-the-moon-cover.jpg",
+    img: "https://res.cloudinary.com/a22sounds/image/upload/v1613017396/videos/hitting-the-moon-cover_yfmrho.jpg",
     release_date: "February 28, 2020",
     youtube_embed: "https://www.youtube.com/embed/_rlrfH4UKYQ?vq=hd1080&modestbranding=1&theme=light&color=white",
     url: "https://www.youtube.com/watch?v=_rlrfH4UKYQ"
   },
   {
     title: "Starburst - Single",
-    img: "/assets/img/videos/starburst-cover.jpg",
+    img: "https://res.cloudinary.com/a22sounds/image/upload/v1613017396/videos/starburst-cover_jxvfdr.jpg",
     release_date: "October 9, 2020",
     youtube_embed: "https://www.youtube.com/embed/-ywU94Dxurs?vq=hd1080&modestbranding=1&theme=light&color=white",
     url: "https://www.youtube.com/watch?v=-ywU94Dxurs"
   },
   {
     title: "Hitting the Moon - Single",
-    img: "/assets/img/videos/hitting-the-moon-cover.jpg",
+    img: "https://res.cloudinary.com/a22sounds/image/upload/v1613017396/videos/hitting-the-moon-cover_yfmrho.jpg",
     release_date: "February 28, 2020",
     youtube_embed: "https://www.youtube.com/embed/_rlrfH4UKYQ?vq=hd1080&modestbranding=1&theme=light&color=white",
     url: "https://www.youtube.com/watch?v=_rlrfH4UKYQ"
   },
   {
     title: "Starburst - Single",
-    img: "/assets/img/videos/starburst-cover.jpg",
+    img: "https://res.cloudinary.com/a22sounds/image/upload/v1613017396/videos/starburst-cover_jxvfdr.jpg",
     release_date: "October 9, 2020",
     youtube_embed: "https://www.youtube.com/embed/-ywU94Dxurs?vq=hd1080&modestbranding=1&theme=light&color=white",
     url: "https://www.youtube.com/watch?v=-ywU94Dxurs"
   },
   {
     title: "Hitting the Moon - Single",
-    img: "/assets/img/videos/hitting-the-moon-cover.jpg",
+    img: "https://res.cloudinary.com/a22sounds/image/upload/v1613017396/videos/hitting-the-moon-cover_yfmrho.jpg",
     release_date: "February 28, 2020",
     youtube_embed: "https://www.youtube.com/embed/_rlrfH4UKYQ?vq=hd1080&modestbranding=1&theme=light&color=white",
     url: "https://www.youtube.com/watch?v=_rlrfH4UKYQ"
   },
   {
     title: "Starburst - Single",
-    img: "/assets/img/videos/starburst-cover.jpg",
+    img: "https://res.cloudinary.com/a22sounds/image/upload/v1613017396/videos/starburst-cover_jxvfdr.jpg",
     release_date: "October 9, 2020",
     youtube_embed: "https://www.youtube.com/embed/-ywU94Dxurs?vq=hd1080&modestbranding=1&theme=light&color=white",
     url: "https://www.youtube.com/watch?v=-ywU94Dxurs"
   },
   {
     title: "Hitting the Moon - Single",
-    img: "/assets/img/videos/hitting-the-moon-cover.jpg",
+    img: "https://res.cloudinary.com/a22sounds/image/upload/v1613017396/videos/hitting-the-moon-cover_yfmrho.jpg",
     release_date: "February 28, 2020",
     youtube_embed: "https://www.youtube.com/embed/_rlrfH4UKYQ?vq=hd1080&modestbranding=1&theme=light&color=white",
     url: "https://www.youtube.com/watch?v=_rlrfH4UKYQ"
   },
   {
     title: "Starburst - Single",
-    img: "/assets/img/videos/starburst-cover.jpg",
+    img: "https://res.cloudinary.com/a22sounds/image/upload/v1613017396/videos/starburst-cover_jxvfdr.jpg",
     release_date: "October 9, 2020",
     youtube_embed: "https://www.youtube.com/embed/-ywU94Dxurs?vq=hd1080&modestbranding=1&theme=light&color=white",
     url: "https://www.youtube.com/watch?v=-ywU94Dxurs"
   },
   {
     title: "Hitting the Moon - Single",
-    img: "/assets/img/videos/hitting-the-moon-cover.jpg",
+    img: "https://res.cloudinary.com/a22sounds/image/upload/v1613017396/videos/hitting-the-moon-cover_yfmrho.jpg",
     release_date: "February 28, 2020",
     youtube_embed: "https://www.youtube.com/embed/_rlrfH4UKYQ?vq=hd1080&modestbranding=1&theme=light&color=white",
     url: "https://www.youtube.com/watch?v=_rlrfH4UKYQ"
