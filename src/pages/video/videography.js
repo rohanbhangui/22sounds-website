0d0947d93dad62f80610925a125a243a3302810b
Rohan Bhangui <bhanguirohan@gmail.com>
2021-02-16 19:10:15 -0500

    changes to layout for music and video

--- src/pages/video/videography.js
+++ src/pages/video/videography.js
@@ -1,13 +1,13 @@
 const full = [
   {
-    title: "Where Do We Go - Single",
+    title: "Where Do We Go",
     img: "https://res.cloudinary.com/a22sounds/image/upload/v1613519880/videos/where-do-we-go-cover_m57gzx.jpg",
     release_date: "February 18, 2021",
     youtube_embed: "https://www.youtube.com/embed/T24OD5QkXUM?vq=hd1080&modestbranding=1&theme=light&color=white",
     url: "https://www.youtube.com/watch?v=T24OD5QkXUM"
   },
   {
-    title: "Starburst - Single",
+    title: "Starburst",
     img: "https://res.cloudinary.com/a22sounds/image/upload/v1613017396/videos/starburst-cover_jxvfdr.jpg",
     release_date: "October 9, 2020",
     youtube_embed: "https://www.youtube.com/embed/-ywU94Dxurs?vq=hd1080&modestbranding=1&theme=light&color=white",
