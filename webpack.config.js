1427564d635284c7483f651db9ead6b479c229e9
Rohan Bhangui <bhanguirohan@gmail.com>
2021-02-18 22:14:57 -0500

    fixed the 404 page

--- webpack.config.js
+++ webpack.config.js
@@ -115,7 +115,8 @@ module.exports = {
     }),
     new CopyPlugin({
       patterns: [
-        { from: "src/assets/img", to: 'assets/img' },
+        { from: "src/assets/img", to: "assets/img" },
+        { from: "src/404.html", to: "/404.html" },
       ],
     }),
   ]
